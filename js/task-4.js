a78a992f55593b84440fc504f25f147b6c2016bd
Dmytro S. <dp270788sdo@gmail.com>
2026-04-22 17:56:07 -0230

    ver 1.1

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const { email, password } = event.target.elements;
+
+
+
+
+
+
+
+}
