bdee3555159de851e16da5759b7e44bf4cb0d7c7
Dmytro S. <dp270788sdo@gmail.com>
2026-04-22 17:46:00 -0230

    ver 1.2

--- js/task-2.js
+++ js/task-2.js
@@ -27,3 +27,16 @@ const images = [
     alt: 'Zebras on Zebra',
   },
 ];
+
+
+const gallery = document.querySelector('.gallery');
+
+const markup = images
+  .map(image => {
+    return `<li>
+      <img src="${image.url}" alt="${image.alt}">
+    </li>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
